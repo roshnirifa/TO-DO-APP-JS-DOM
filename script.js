6b0d790cddb5ae71cff5fe271f412a3703cd4f2d
roshnirifa <roshnirifa998@gmail.com>
2022-12-31 22:19:11 +0600

    delete btn added

--- script.js
+++ script.js
@@ -11,15 +11,34 @@ document.getElementById('addTaskBtn').addEventListener('click', function (e) {
     input.type = 'checkbox';
     let label = document.createElement('label');
     let li = document.createElement('li');
+    let btn = document.createElement('button');
+    btn.innerText = 'X';
+    btn.className = 'deleteBtn'
     label.innerText = newTaskValue;
 
     let taskContainer = document.getElementById('task');
     taskContainer.appendChild(li)
     taskContainer.appendChild(input)
     taskContainer.appendChild(label)
+    taskContainer.appendChild(btn)
 
 
 })
+// // delete btn
+// let delBtns = document.getElementsByClassName('deleteBtn');
+// // console.log(delBtns);
+// for (let delBtn of delBtns) {
+//     // console.log(delBtn);
+//     delBtn.addEventListener('click', function () {
+//         console.log('fgds');
+//         let taskContainer = document.getElementById('task');
+//         taskContainer.style.display = 'none'
+
+//     })
+// }
+
+
+
 
 
 
